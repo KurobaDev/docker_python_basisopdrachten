3c8c6bdd9291c364fd07ce737a78e6a35e07b079
KurobaDev <pepijnklaver@gmail.com>
2026-09-17 16:04:39 +0200

    hfstberekeningen_opdr1_opdr2

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,13 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+opgeteldeLeeftijd = joe + bob + fred
+
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {opgeteldeLeeftijd}")
+
+print(f"De gemiddelde leeftijd van deze boys is: {opgeteldeLeeftijd / 3}")
--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,14 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+print(f"{c} graden Celcius is gelijk aan {(c * 1.8) + 32} graden Fahrenheit")
+print(f"{f} gradenn Fahrenheit is gelijk aan {(f - 32) / 1.8} graden Celsius")
+
+c = 62.2
+f = 96
+
+print(f"{c} graden Celsius is gelijk aan {(c * 1.8) + 32} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {(f - 32) / 1.8} graden Celsius")
