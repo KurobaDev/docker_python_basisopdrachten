fb2d04a91deb5e2e7cc431d01f3d0daacb322fbe
KurobaDev <pepijnklaver@gmail.com>
2026-09-24 14:11:01 +0200

    hfst30_03&40

--- app/030_berekeningen/opdr_3/opdr_3.py
+++ app/030_berekeningen/opdr_3/opdr_3.py
@@ -1,16 +1,11 @@
-# Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
-
-# Hier komt je code...
 x = 1
-y = ...
-print( ... )
+y = 4 * x**3 - (2 * x**2 - 1)
+print(f"De uitkomst is: {y}")
 
 x = 2
-y = ...
-print( ... )
+y = 4 * x**3 - (2 * x**2 - 1)
+print(f"De uitkomst is: {y}")
 
 x = 0
-y = ...
-print( ... )
+y = 4 * x**3 - (2 * x**2 - 1)
+print(f"De uitkomst is: {y}")
